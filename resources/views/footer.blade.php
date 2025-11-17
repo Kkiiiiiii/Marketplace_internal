40bb47c0547c8b94738200813a59c50ac8c05b9c
Kkiiiiiii <rikizhakaria@gmail.com>
2025-11-17 13:33:38 +0700

    memperbaiki footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,52 +1,62 @@
 <footer class="text-white text-center bg-utama py-4">
     <div class="container">
-        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mb-3 gap-3">
+        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mb-4 gap-3">
             <div class="d-flex align-items-center gap-2">
                 <i class="fa fa-store" style="font-size: 50px;"></i>
-                <h6>Marketplace SMK</h6>
+                <h6 class="m-0">Marketplace SMK</h6>
             </div>
+
             <div class="d-flex gap-4">
                 <div class="footer-social-icon">
-                     <a href="https://www.facebook.com/share/1ZZrxKnyzo/" class="text-dark text-decoration-none" aria-label="Facebook">
+                    <a href="https://www.facebook.com/share/1ZZrxKnyzo/"
+                       class="text-dark text-decoration-none" aria-label="Facebook">
                         <i class="fab fa-facebook-f"></i>
                     </a>
                 </div>
+
                 <div class="footer-social-icon">
-                     <a href="https://youtube.com/?si=9KtIhuPxcLjHKNQl" class="text-dark text-decoration-none" aria-label="Youtube">
+                    <a href="https://youtube.com/?si=9KtIhuPxcLjHKNQl"
+                       class="text-dark text-decoration-none" aria-label="Youtube">
                         <i class="fab fa-youtube"></i>
                     </a>
                 </div>
+
                 <div class="footer-social-icon">
-                     <a href="https://www.instagram.com/smpn.duagunungputri?igsh=N3JqcTk4YzRkZ2ww" class="text-dark text-decoration-none" aria-label="Intagram">
+                    <a href="https://www.instagram.com/smpn.duagunungputri?igsh=N3JqcTk4YzRkZ2ww"
+                       class="text-dark text-decoration-none" aria-label="Instagram">
                         <i class="fab fa-instagram"></i>
                     </a>
                 </div>
             </div>
         </div>
 
-        <div class="row mt-5 d-flex">
-            <div class="col-md-6 text-start">
-              <h6>Informasi Singkat</h6>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil perfer
-                endis nisi recusandae nulla saepe, omnis sed voluptate
-                 enim iure beatae? Id, quisquam autem fuga saepe illo debitis similique. Modi beatae volupt
-                 atum numquam labore saepe ex qui amet quibusdam quia cum ducimus laboriosam quisquam eum, doloribus possimus, perferendis ad consectetur?</p>
-            </div>
-            <div class="col-md-12">
-                <h6 class="mb-1">Kontak</h6>
-                        <p class="small mb-1">Telp: 02186868677</p>
-                        <p class="small mb-0">Email: info@smpn02.sch.id</p>
-                        <br>
-                        <h6 class="mb-1">Informasi Singkat</h6>
-                        <p class="small mb-1">Tempat Jual Beli Bagi Warga Sekolah</p>
-                        <p class="small mb-0"></p>
-                        <p class="small mb-0">Penyelengaraan: Sehari Penuh/5 hari</p>
-
-            </div>
-        </div>
+        <div class="row text-start mt-4">
+            <div class="col-md-6 mb-4">
+                <h6 class="fw-bold">Informasi Singkat</h6>
+                <p class="small">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil perferendis
+                    nisi recusandae nulla saepe, omnis sed voluptate enim iure beatae? Id, quisquam
+                    autem fuga saepe illo debitis similique.
+                </p>
             </div>
 
-        <div class="container-fluid bg-cust">
-            <p class="mt-3 mb-1 small"><strong>Copyright</strong> &copy; 2025 - <strong>Hak Cipta Dilindungi.</strong></p>
+            <div class="col-md-6 mb-4">
+                <h6 class="fw-bold">Kontak</h6>
+                <p class="small mb-1">Telp: 02186868677</p>
+                <p class="small mb-3">Email: info@smpn02.sch.id</p>
+
+                <h6 class="fw-bold">Informasi Tambahan</h6>
+                <p class="small mb-1">Tempat Jual Beli Bagi Warga Sekolah</p>
+                <p class="small mb-0">Penyelenggaraan: Sehari Penuh / 5 Hari</p>
+            </div>
         </div>
+
+    </div>
+
+    <div class="container-fluid bg-cust copyright-bar">
+        <p class="m-0 small text-center py-3">
+            <strong>Copyright</strong> © 2025 -
+            <strong>Hak Cipta Dilindungi.</strong>
+        </p>
+    </div>
 </footer>
